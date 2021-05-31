7c708c9b5502cdca728adea5cd0d14c4422255d0
Hitsuji-otoko <shmahoro@gmail.com>
2021-05-31 18:29:10 +0900

    プロジェクトの作成

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <router-view />
 </template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
 
 <style>
 #app {
