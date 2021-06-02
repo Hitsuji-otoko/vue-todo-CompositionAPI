166d319a8a829c3eb285f2273765aa245c2c0029
Hitsuji-otoko <shmahoro@gmail.com>
2021-06-02 12:24:21 +0900

    ストアからRepositoryを利用するように変更

--- src/store/todo/index.ts
+++ src/store/todo/index.ts
@@ -1,48 +1,44 @@
 import { InjectionKey, reactive, readonly } from 'vue'
 import { Params, Todo, TodoState, TodoStore } from '@/store/todo/types'
+import Repository, { TODOS } from '@/clients/TodoClient/RepositoryFactory'
 
-//まだ永続化の処理を実装していないため、とりあえずモックとして用意
-const mockTodo: Todo[] = [
-  {
-    id: 1,
-    title: 'todo1',
-    description: '1つ目',
-    status: 'waiting',
-    createdAt: new Date('2020-12-01'),
-    updatedAt: new Date('2020-12-01'),
-  },
-  {
-    id: 2,
-    title: 'todo2',
-    description: '2つ目',
-    status: 'waiting',
-    createdAt: new Date('2020-12-02'),
-    updatedAt: new Date('2020-12-02'),
-  },
-  {
-    id: 3,
-    title: 'todo3',
-    description: '3つ目',
-    status: 'working',
-    createdAt: new Date('2020-12-03'),
-    updatedAt: new Date('2020-12-04'),
-  },
-]
+const TodoRepository = Repository[TODOS]
 
 const state = reactive<TodoState>({
-  todos: mockTodo
+  todos: [
+    {
+      id: 1,
+      title: 'todo1',
+      description: '1つ目',
+      status: 'waiting',
+      createdAt: new Date('2020-12-01'),
+      updatedAt: new Date('2020-12-01'),
+    },
+    {
+      id: 2,
+      title: 'todo2',
+      description: '2つ目',
+      status: 'waiting',
+      createdAt: new Date('2020-12-02'),
+      updatedAt: new Date('2020-12-02'),
+    },
+    {
+      id: 3,
+      title: 'todo3',
+      description: '3つ目',
+      status: 'working',
+      createdAt: new Date('2020-12-03'),
+      updatedAt: new Date('2020-12-04'),
+    },
+  ],
 })
+const fetchTodos = async () => {
+  state.todos = await TodoRepository.getAll()
+}
 
-const initializeTodo = (todo: Params) => {
-  const date = new Date()
-  return {
-    id: date.getTime(),
-    title: todo.title,
-    description: todo.description,
-    status: todo.status,
-    createdAt: date,
-    updatedAt: date
-  } as Todo
+const fetchTodo = async (id: number) => {
+  const todo = await TodoRepository.get(id)
+  state.todos.push(todo)
 }
 
 const getTodo = (id: number) => {
@@ -53,24 +49,29 @@ const getTodo = (id: number) => {
   return todo
 }
 
-const addTodo = (todo: Params) => {
-  state.todos.push(initializeTodo(todo))
+const addTodo = async (todo: Params) => {
+  const result = await TodoRepository.create(todo)
+  state.todos.push(result)
 }
 
-const updateTodo = (id: number, todo: Todo) => {
+const updateTodo = async (id: number, todo: Todo) => {
+  const result = await TodoRepository.update(id, todo)
   const index = state.todos.findIndex((todo) => todo.id === id)
   if (index === -1) {
     throw new Error(`cannot find todo by id:${id}`)
   }
-  state.todos[index] = todo
+  state.todos[index] = result
 }
 
 const deleteTodo = (id: number) => {
+  TodoRepository.delete(id)
   state.todos = state.todos.filter((todo) => todo.id !== id)
 }
 
 const todoStore: TodoStore = {
   state: readonly(state),
+  fetchTodos,
+  fetchTodo,
   getTodo,
   addTodo,
   updateTodo,
